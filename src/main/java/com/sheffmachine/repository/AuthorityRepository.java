c811a3d5184e89dcdee61f621f227b78bef56c66
SHEFFmachine <sheff@sheffmachine.com>
2019-02-21 19:38:20 -0800

    Initial application generated by JHipster-5.8.1

---
+++ src/main/java/com/sheffmachine/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.sheffmachine.repository;
+
+import com.sheffmachine.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the Authority entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
---
+++ src/main/java/com/sheffmachine/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.sheffmachine.service.dto;
